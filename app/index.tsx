f57b7e74d4f86d5ceeb958ff46beb54b1e7470c4
njxue <ngjxue@gmail.com>
2025-01-18 11:05:49 +0800

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-black">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
